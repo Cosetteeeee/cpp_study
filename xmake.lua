edd403f403e2169e7441cc9d7054dd41008ba4a8
cosette <2052137445@qq.com>
2025-09-23 20:21:25 +0800

    init

--- xmake.lua
+++ xmake.lua
@@ -18,7 +18,7 @@ add_shflags("--target=x86_64-w64-mingw32", "-stdlib=libc++")
 add_ldflags("--target=x86_64-w64-mingw32", "-stdlib=libc++", {force = true})
 
 -- 添加MinGW头文件路径
-add_includedirs("C:/Users/cosette/scoop/apps/llvm-mingw/current/include/c++/v1")
+
 add_includedirs("C:/Users/cosette/scoop/apps/llvm-mingw/current/include")
 
 -- 添加库搜索路径
@@ -35,3 +35,10 @@ target("addressbook")
     add_files("addressbook/*.cpp")
     add_files("addressbook/libs/*.cpp")
     add_headerfiles("addressbook/include/*.hpp")
+    add_includedirs("C:/Users/cosette/scoop/apps/llvm-mingw/current/include/c++/v1")
+target("c")
+    set_kind("binary")
+
+    add_files("c/*.c")
+    add_files("c/libs/*.c")
+    add_headerfiles("c/include/*.h")
